25383c3a74847c97d0c1cd519cf79299789baff6
Nicholas G Reich <nick@schoolph.umass.edu>
2014-06-20 16:31:02 -0400

    adding code with memory terms broken apart.

---
+++ memory/fourStrainMemory_breakout.R
@@ -0,0 +1,130 @@
+#################################
+## Create 4-strain memory term ##
+#################################
+
+createMemory <- function(dat, k, lambda=NULL) {
+        ## assume that dat is data from a four-strain model, columns are case counts
+        ## k is a duration
+        ## lambda is an exponential parameter
+        ## first, we will create a matrix to help with the memory term summations.
+        ## this matrix will have 0s and 1s and will be multiplied by the data vectors
+        
+        ## vector of 1s of length k
+        if(is.null(lambda)) {
+                vec1 <- rep(1, k)
+        } else {
+                ## rough cumulative probabilities of still being susceptible
+                vec1 <- 1-pexp((k-1):0, rate=1/lambda)
+                ## remove most recent kstar counts from consideration to avoid autocorrelation
+                kstar <- 10
+                vec1[(k-kstar+1):k] <- 0
+        }
+        ## vector of 0s of length ncol(dat)-k+1
+        vec0 <- rep(0, nrow(dat)-k+1)
+        memMatrixRows <- nrow(dat)-k
+
+        ## the matrix for self strain
+        firstReps <- rep(c(rep(1, k), vec0), times=memMatrixRows-1)
+        last1s <- c(rep(1, k),0)
+        memMatrixSelf <- matrix(c(firstReps, last1s), byrow=TRUE, nrow=memMatrixRows)
+        
+        ## the matrix for other strains
+        firstReps <- rep(c(vec1, vec0), times=memMatrixRows-1)
+        last1s <- c(vec1,0)
+        memMatrix <- matrix(c(firstReps, last1s), byrow=TRUE, nrow=memMatrixRows)
+        ## strain 1 calculations
+        M1_self <- memMatrixSelf%*%dat[,1] 
+        M1_self <- ( M1_self-mean(M1_self) ) / sd(M1_self)
+        M1 <- memMatrix%*%dat[,2] + memMatrix%*%dat[,3] + memMatrix%*%dat[,4]  
+        M1 <- ( M1-mean(M1) ) / sd(M1)
+        ## strain 2 calculations
+        M2_self <- memMatrixSelf%*%dat[,2] 
+        M2_self <- ( M2_self-mean(M2_self) ) / sd(M2_self)
+        M2 <- memMatrix%*%dat[,1] + memMatrix%*%dat[,3] + memMatrix%*%dat[,4]  
+        M2 <- ( M2-mean(M2) ) / sd(M2)
+        ## strain 3 calculations
+        M3_self <- memMatrixSelf%*%dat[,3] 
+        M3_self <- ( M3_self-mean(M3_self) ) / sd(M3_self)
+        M3 <- memMatrix%*%dat[,1] + memMatrix%*%dat[,2] + memMatrix%*%dat[,4]  
+        M3 <- ( M3-mean(M3) ) / sd(M3)
+        ## strain 4 calculations
+        M4_self <- memMatrixSelf%*%dat[,4] 
+        M4_self <- ( M4_self-mean(M4_self) ) / sd(M4_self)
+        M4 <- memMatrix%*%dat[,1] + memMatrix%*%dat[,2] + memMatrix%*%dat[,3]
+        M4 <- ( M4-mean(M4) ) / sd(M4)
+        
+        dataIdx <- (k+1):nrow(dat)
+        dat1 <- data.frame(y=dat[dataIdx,1], 
+                           yAR = dat[dataIdx-1, 1],
+                           M=M1, 
+                           Mself=M1_self,
+                           time=1:length(dataIdx),
+                           strain=1)
+        dat2 <- data.frame(y=dat[dataIdx,2], 
+                           yAR = dat[dataIdx-1, 2],
+                           M=M2, 
+                           Mself=M2_self,
+                           time=1:length(dataIdx),
+                           strain=2)
+        dat3 <- data.frame(y=dat[dataIdx,3], 
+                           yAR = dat[dataIdx-1, 3],
+                           M=M3, 
+                           Mself=M3_self,
+                           time=1:length(dataIdx),
+                           strain=3)
+        dat4 <- data.frame(y=dat[dataIdx,4], 
+                           yAR = dat[dataIdx-1, 4],
+                           M=M4, 
+                           Mself=M4_self,
+                           time=1:length(dataIdx),
+                           strain=4)
+        data <- rbind(dat1, dat2, dat3, dat4)
+        return(data)
+}
+
+
+runCrossProtectMemoryAnalysis <- function(data, subset=NULL, k, max_lambda, plot=FALSE, verbose=TRUE) {
+        if(!is.null(subset)){
+                data <- data[subset,]
+        }
+        nLam <- max_lambda+1
+        logLiks <- matrix(NA, ncol=6, nrow=nLam)
+        colnames(logLiks) <- c("k", "lambda", "loglik", "z", "beta_M", "beta_Mself")
+        for(i in 1:nLam){
+                logLiks[i,"k"] <- k
+                lambda <- logLiks[i,"lambda"] <- i
+                dat <- createMemory(data, k=k, lambda=lambda)
+                dat$t <- 1:nrow(dat)
+                m1 <- glm(y ~ factor(strain) + log(yAR+1) + M + Mself, data=dat, family="poisson")
+                logLiks[i,"z"] <- summary(m1)$coef["M","z value"] #logLik(m1)
+                logLiks[i,"loglik"] <- logLik(m1)
+                logLiks[i,"beta_M"] <- summary(m1)$coef["M","Estimate"]
+                logLiks[i,"beta_Mself"] <- summary(m1)$coef["Mself","Estimate"]
+                if(verbose)
+                        message(paste("finished iteration", i, Sys.time()))
+        }
+        
+        if(plot) {
+                require(gridExtra)
+                p1 <- qplot(lambda, loglik, data=data.frame(logLiks), geom="line")
+                #p2 <- qplot(lambda, z, data=data.frame(logLiks), geom="line")       
+                #p3 <- qplot(lambda, beta_M, data=data.frame(logLiks), geom="line")       
+                grid.arrange(p1, ncol=1)
+        }
+        
+        ## find "best" lambda
+        idx_best_ll <- which(logLiks[,"loglik"]==max(logLiks[,"loglik"]))
+        if(length(idx_best_ll)!=1) {
+                warning("multiple log-likelihood maxima. first max selected.")
+                idx_best_ll <- min(idx_best_ll)
+        }
+        
+        
+        ## compare likelihoods obtain chi-square based confidence interval
+        chisq_threshold <- qchisq(.95, df=1)/2
+        idx_ci_ll <- which(logLiks[,"loglik"]>=logLiks[idx_best_ll,"loglik"]-chisq_threshold)
+        ci_low <- min(logLiks[idx_ci_ll,"lambda"])
+        ci_high <- max(logLiks[idx_ci_ll,"lambda"])
+        
+        return(c(logLiks[idx_best_ll,], lambda_ci_low=ci_low, lambda_ci_high=ci_high))
+}
---
+++ memory/memorySimulation_breakout.R
@@ -0,0 +1,100 @@
+## Memory cross-protection analysis simulation
+## Nicholas Reich
+## May 2014
+
+## preliminaries
+setwd('~/Documents/code_versioned/pompSandbox/memory/')
+source('~/Documents/code_versioned/pompSandbox/memory/fourStrainMemory.R')
+path <- "~/Documents/work/dengueData/simdata_5_local/"
+key <- read.csv("~/Documents/work/research/dengueCrossProtection/data/simulatedData_fromMI/key_5.csv")
+colnames(key)[1] <- "filename"
+
+## parameters
+k <- 300
+lambda <- 26*4
+file_idx <- 1:12000 #c(701:750, 4681:4730, 2911:2934, 2936:2961)
+n_files <- length(file_idx)
+
+## storage
+results <- matrix(NA, ncol=12, nrow=n_files)
+colnames(results) <- c(colnames(key), "k", "lambda", "loglik", "z", "beta_M", "beta_Mself", "lambda_ci_low", "lambda_ci_high")
+results[,1] <- file_idx
+results[,2:4] <- as.matrix(key[file_idx,2:4])
+
+## create loop for each filename
+require(doMC)
+registerDoMC(18)
+tic <- Sys.time()
+parResults <- foreach(i = 1:n_files, .combine=rbind) %dopar% {
+        fn <- paste0(path, key[file_idx[i],"filename"])
+        raw_data <- read.csv(fn, row.names=1)
+        tsir_data <- raw_data[,2:5]
+        runCrossProtectMemoryAnalysis(data=tsir_data, k=k, 
+                                      subset=500:1041,
+                                      max_lambda=lambda, 
+                                      plot=FALSE, verbose=FALSE)
+}
+toc <- Sys.time()
+toc-tic
+results[,5:12] <- parResults
+
+write.csv(results, file="simResults.csv", quote=FALSE, row.names=FALSE)
+
+#results <- read.csv("simResults_20140611.csv")
+
+results <- as.data.frame(results)
+results$trueLambda <- 26/results$delta
+results$memoryIndicator <- results$lambda>13# abs(results$beta_M)>.02
+results$ciCover <- results$trueLambda>=results$lambda_ci_low & results$trueLambda<=results$lambda_ci_high
+results$ciWidth <- results$lambda_ci_high-results$lambda_ci_low
+results$bias <- results$lambda - results$trueLambda
+results$cpPresent <- results$lambda_ci_low>0
+with(results, table(trueLambda, ciCover))/2000
+with(results, table(trueLambda, cpPresent))/2000
+
+#results
+qplot(factor(trueLambda), lambda, data=results, geom="violin")
+qplot(factor(trueLambda), lambda, data=results, geom="boxplot", facets=.~rho)
+qplot(factor(trueLambda), lambda_ci_high, data=results, geom="boxplot", facets=.~rho)
+qplot(factor(trueLambda), lambda_ci_low, data=results, geom="boxplot", facets=.~rho)
+qplot(factor(trueLambda), ciWidth, data=results, geom="boxplot", facets=.~rho)
+qplot(factor(trueLambda), ciWidth, data=results, geom="violin", facets=.~rho)
+
+qplot(factor(trueLambda), bias/trueLambda, data=results, geom="boxplot", facets=.~rho) + ylim(-3,3)
+
+qplot(factor(trueLambda), z, data=results, geom="violin")
+qplot(factor(trueLambda), z, data=results, geom="boxplot")
+qplot(factor(trueLambda), sign(z)*lambda, data=results, geom="boxplot")
+qplot(factor(trueLambda), beta_M, data=results, geom="boxplot")
+qplot(factor(trueLambda), beta_M, data=results, geom="violin")
+qplot(factor(memoryIndicator), data=results, geom="bar", fill=factor(trueLambda))
+
+## run Dengue analysis
+load("/Users/nick/Dropbox/work/research/dengueCrossProtection/data/bkk.dengue.cases.new.rda")
+dengue_data <- bkk.dengue.cases[,3:6]
+dengue_data[which(dengue_data<0, arr.ind=TRUE)] <- 0 ## remove negative values
+tmp <- runCrossProtectMemoryAnalysis(data=dengue_data, k=300, 
+                                     max_lambda=26*6, 
+                                     plot=TRUE, verbose=FALSE)
+
+## run flu A, flu B, RSV and paraflu
+rsv_data <- read.csv("/Users/nick/Dropbox/work/research/maskStudy/manuscripts/ALERTv2/dataCleaned/chco.csv")
+rsv_data_subset <- rsv_data[,c("RSV", "Total.Flu.A", "Flu.B", "Paraflu")]
+tmp <- runCrossProtectMemoryAnalysis(data=rsv_data_subset, k=120, 
+                                     max_lambda=100, 
+                                     plot=TRUE, verbose=FALSE)
+
+rsv_data_subset <- rsv_data[,c("RSV", "Total.Flu.A", "Flu.B", "Rhinovirus")]
+tmp <- runCrossProtectMemoryAnalysis(data=rsv_data_subset, k=120, 
+                                     max_lambda=100, 
+                                     plot=TRUE, verbose=FALSE)
+
+rsv_data_subset <- rsv_data[,c("RSV", "Total.Flu.A", "Flu.B", "Adenovirus")]
+tmp <- runCrossProtectMemoryAnalysis(data=rsv_data_subset, k=120, 
+                                     max_lambda=100, 
+                                     plot=TRUE, verbose=FALSE)
+
+rsv_data_subset <- rsv_data[,c("RSV", "Total.Flu.A", "Flu.B", "Enterovirus")]
+tmp <- runCrossProtectMemoryAnalysis(data=rsv_data_subset, k=120, 
+                                     max_lambda=100, 
+                                     plot=TRUE, verbose=FALSE)
