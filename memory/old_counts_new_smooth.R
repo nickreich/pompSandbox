736c44164db049987a8f978e9fbc7d2030c8fe25
Nicholas Reich <nick@schoolph.umass.edu>
2014-08-11 13:55:28 -0400

    working draft of interpolation code for QS data.

--- memory/old_counts_new_smooth.R
+++ memory/old_counts_new_smooth.R
@@ -3,6 +3,8 @@
 ## August 2014
 
 library(dplyr)
+library(reshape2)
+library(parallel)
 
 ## Get data.
 counts <- tbl_df(read.csv("/Volumes/projects/Projects/Dengue/QSData/QSNICH_data_1973_2014.csv"))
@@ -21,91 +23,72 @@ grouped_counts <- counts %>%
 
 # qplot(date, ymax=cases, ymin=0, geom="linerange", data=grouped_counts, facets=serotype~.)
 
+## make complete data structure
+empty_data <- tbl_df(expand.grid(month=1:12, year=1973:2012, serotype=1:4))
+full_data <- left_join(empty_data, grouped_counts) %>%
+        mutate(date = year + (month-1)/12) %>%
+        filter(date>=1973.5) 
+full_data[is.na(full_data$cases), "cases"] <- 0
+        
+# qplot(date, ymax=cases, ymin=0, geom="linerange", data=full_data, facets=serotype~.)
 
-split_counts <- split(x=old_counts, f=old_counts[['province']])
+## make cumulative counts
+full_data <- full_data %>% 
+        arrange(date) %>%
+        group_by(serotype) %>%
+        mutate(cum_cases = cumsum(cases)) %>%
+        ungroup()
+ 
 
-split_counts <- mclapply(
-	X=split_counts,
-	FUN=function(counts) {
-		counts_anchor <- data.frame(
-			year = min_year, month = 0, 
-			province_name = counts[['province_name']][1],
-			province = counts[['province']][1], 
-			population = counts[['population']][1],
-			area_km_2 = counts[['area_km_2']][1],
-			count = 0
-		)
-		counts <- rbind(counts_anchor,counts)
-		counts <- counts[order(counts[['year']], counts[['month']]),]
-		months_are_sequential <- all(diff(counts[['month']]) %in% c(1,-11,NA))
-		years_are_sequential <- all(diff(counts[['year']]) %in% c(1,0,NA))
-		if (!months_are_sequential) 
-			return('Check that all months are available.')
-		if (!years_are_sequential) 
-			return('Check that all years are available.')
-		use_these <- counts[['count']]
-		use_these[is.na(use_these)] <- 0
-		counts[['cumulative_count']] <- cumsum(use_these)
-		return(counts)
-	}
-)
+## make spline functions for interpolation
+split_counts <- vector(mode="list", length=4)
+for(i in 1:4){
+        split_counts[[i]] <- filter(full_data, serotype==i)
+}
 
 spline_funs <- mclapply(
-	X=split_counts,
-	FUN=function(counts) {
-		times <- counts[['year']] + counts[['month']]/12
-		sf <- splinefun(x=times, y=counts[['cumulative_count']], method="hyman")
-		return(sf)
-	}
+        X=split_counts,
+        FUN=function(counts) {
+                sf <- splinefun(x=counts$date, y=counts$cum_cases, 
+                                method="hyman")
+                return(sf)
+        }
 )
 
-min_year <- min(old_counts[['year']])
-max_year <- max(old_counts[['year']])
-years <- min_year:max_year
+## run interpolation
+years <- 1974:2012
 biweeks <- 1:26
 
-base <- expand.grid(date_sick_biweek = biweeks, year = years)
-base_anchor <- data.frame(
-	date_sick_biweek = 0,
-	year = min_year
-)
-base <- rbind(base_anchor, base)
-base[['times']] <- base[['year']] + base[['date_sick_biweek']]/26
+base <- tbl_df(expand.grid(date_sick_biweek = biweeks, 
+                           year = years, 
+                           serotype=1:4))
+base <- base %>%
+        mutate(date = year + (date_sick_biweek-1)/26,
+               smooth_cum_counts=NA)
+
+for(i in 1:4){
+        f <- spline_funs[[i]]
+        idx <- which(base$serotype==i)
+        base[idx, "smooth_cum_counts"] <- f(x=base[idx, "date"])
+}
 
 
-interpolated_times <- mclapply(
-	X=names(spline_funs),
-	FUN=function(nom, base) {
-		f <- spline_funs[[nom]]
-		base[['province']] <- as.numeric(nom)
-		base[['smooth_cumulative_counts']] <- f(x=base[['times']])
-		base[['smooth_counts']] <-
-			c(NA,diff(base[['smooth_cumulative_counts']]))
-		return(base[2:nrow(base),])
-	},
-	base = base
-)
+biweek_count_data <- base %>% 
+        arrange(date) %>%
+        group_by(serotype) %>%
+        mutate(cases = round(c(0, diff(smooth_cum_counts)))) %>%
+        ungroup()
 
-smooth_counts <- do.call(what=rbind, args=interpolated_times)
+#qplot(date, ymax=cases, ymin=0, geom="linerange", data=biweek_count_data, facets=serotype~.)
 
 
-## Write standardized columns version:
-count_table <- 'biweely_dengue_counts_1968_to_2005'
-dbSendQuery(link$conn,paste0('DROP TABLE IF EXISTS public.', count_table, ';'))
-#dbSendQuery(link$conn,paste0('DROP TABLE IF EXISTS standardized_data.', count_table, ';'))
-dbWriteTable(conn=link$conn, name=count_table,
-             value=smooth_counts, row.names=FALSE, overwrite=TRUE, append=FALSE)
-#dbSendQuery(link$conn,paste0('ALTER TABLE ', count_table, ' SET SCHEMA standardized_data;'))
+## make wide data
+ready_data <- tbl_df(dcast(biweek_count_data, 
+                           date + year + date_sick_biweek ~ serotype, 
+                           value.var="cases"))
 
 
-
-#for ( i in unique(smooth_counts[['province']]) ) {
-#	print(i)
-#	pl <- ggplot(
-#		data=smooth_counts[smooth_counts[['province']] == i,], 
-#		aes(x=times, y=smooth_counts, colour=factor(province))
-#	) + geom_line() 
-#	print(pl) 
-#	readline()
-#}
-
+runCrossProtectMemoryAnalysis(data=ready_data[ready_data$date>1990, 4:7], 
+                              k=200, 
+                              max_lambda=26*6, 
+                              plot=TRUE, verbose=FALSE)
